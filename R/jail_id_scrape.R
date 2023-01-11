752cd146b2eee8da7657b3fedf498a95e03f56e4
jpbranson <jpbranson@jpbransons-iMac.local>
2023-01-11 14:09:10 -0600

    fix from https://github.com/swirldev/swirl/issues/475

--- R/jail_id_scrape.R
+++ R/jail_id_scrape.R
@@ -3,6 +3,8 @@ library(httr)
 library(rvest)
 library(lubridate)
 
+set_config( config( ssl_verifypeer = 0L ) )
+
 
 headers = c(
   `Accept` = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
